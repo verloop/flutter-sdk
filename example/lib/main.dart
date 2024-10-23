e06bc7f2f3d80448a76bc2d3299047f11de6f8fd
Pankaj Patel <patelpankaj.1010@gmail.com>
2024-10-23 09:52:53 +0530

    firebase files updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform,
+      options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
